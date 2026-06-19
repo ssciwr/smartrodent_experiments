b7b6a44541647f438e210c59062543b506eb758b
Harald Mack <harald.mack@protonmail.com>
2026-06-19 16:48:18 +0200

    update process code

--- src/smartrodent/biotrove_process/biotrove_process.py
+++ src/smartrodent/biotrove_process/biotrove_process.py
@@ -1,8 +1,8 @@
-from gen_filtered_shuffled_chunks import FileProcessor, load_config
-from metadata_processor import MetadataProcessor
-from get_imgs import GetImages
-from gen_img_txt_pair import GenImgTxtPair
-from plotting_func import generate_plots
+from .gen_filtered_shuffled_chunks import GenShuffledChunks, load_config
+from .metadata_processor import MetadataProcessor
+from .get_imgs import GetImages
+from .gen_img_txt_pair import GenImgTxtPair
+from .plotting_func import generate_plots
 import asyncio
 
 
@@ -12,12 +12,12 @@ def main():
     config = load_config("config.json")
     params = config.get("metadata_processor_info", {})
     mp = MetadataProcessor(**params)
-    processor.process_all_files()
+    mp.process_all_files()
 
     # processor.process_single_file('path_to_single_file.parquet')
     config = load_config("config.json")
     params = config.get("metadata_filter_and_shuffle_info", {})
-    processor = FileProcessor(**params)
+    processor = GenShuffledChunks(**params)
     processor.process_files()
     mp.process_all_files()
 
