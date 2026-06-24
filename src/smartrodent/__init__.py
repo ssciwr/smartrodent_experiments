cc97023ba78abc5360971504c9e85d12cc51e1a9
Harald Mack <harald.mack@protonmail.com>
2026-06-24 17:03:04 +0200

    add vision language yoloe model

--- src/smartrodent/__init__.py
+++ src/smartrodent/__init__.py
@@ -1,13 +1,13 @@
 """SmartRodent model experiment package."""
 
-from .main import main
-from .main import run_speciesnet
+from .detection import main
+from .detection import run_speciesnet
 from .utils import save_speciesnet_crops
 from .utils import save_speciesnet_previews
 from .utils import short_speciesnet_label
 
 __all__ = [
-    "main",
+    "detection",
     "run_speciesnet",
     "save_speciesnet_crops",
     "save_speciesnet_previews",
