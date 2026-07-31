c0366638e8eab5b19db787f6aaedbe7a6540937f
Harald Mack <harald.mack@protonmail.com>
2026-07-31 15:47:55 +0200

    add test skeleton

---
+++ tests/test_inaturalist.py
@@ -0,0 +1,18 @@
+import pytest
+from smartrodent.inaturalist import InaturalistDataset
+
+
+def test_inaturalist_constructor():
+    pass
+
+
+def test_inaturalist_inconsistent_years_def():
+    pass
+
+
+def test_inaturalist_from_config():
+    pass
+
+
+def test_inaturalist_download():
+    pass
